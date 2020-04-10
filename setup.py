6a468ce3e31685ba211b461e21b0a529117fd781
Kaung Htet San <kevin31.kj@gmail.com>
2020-04-10 23:39:42 +0630

    Initial release

--- setup.py
+++ setup.py
@@ -4,14 +4,15 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="pyidaungsu-kaunghtetsan",
-    version="0.0.1",
+    name="pyidaungsu",
+    version="0.0.2",
     author="Kaung Htet San",
     author_email="kaung@htetsan.me",
     description="Python library for Myanmar language",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/kaunghtetsan275/pyidaungsu",
+    download_url = 'https://github.com/kaunghtetsan275/pyidaungsu/archive/0.0.2.tar.gz',
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
