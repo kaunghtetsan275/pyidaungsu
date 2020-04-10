dad3c91544d48f02e7dbc64d95a3a3a09613fdad
Kaung Htet San <kevin31.kj@gmail.com>
2020-04-10 23:18:46 +0630

    initial release

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="Python library for Myanmar language",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/kaunghtetsan275/pyidaungsu",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
