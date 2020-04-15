a7fd041090ab9430d64635c1682be041ff5f3718
Kaung Htet San <kevin31.kj@gmail.com>
2020-04-15 16:48:09 +0630

    version 0.0.7

--- README.md
+++ README.md
@@ -32,17 +32,30 @@ pds.cvt2uni("ထမင္းစားၿပီးၿပီလား")
 >> "ထမင်းစားပြီးပြီလား"
 ```
 
-### Syllabification
+### Tokenization
 
 ```sh
-# syllabification
-pds.syllabify("Alan TuringကိုArtificial Intelligenceနဲ့Computerတွေရဲ့ဖခင်ဆိုပြီးလူသိများပါတယ်")
+# syllable level tokenization for Burmese
+pds.tokenize("Alan TuringကိုArtificial Intelligenceနဲ့Computerတွေရဲ့ဖခင်ဆိုပြီးလူသိများပါတယ်") # lang parameter for default function is 'mm'
 >> ['Alan', 'Turing', 'ကို', 'Artificial', 'Intelligence', 'နဲ့', 'Computer', 'တွေ', 'ရဲ့', 'ဖ', 'ခင်', 'ဆို', 'ပြီး', 'လူ', 'သိ', 'များ', 'ပါ', 'တယ်']
+
+# syllable level tokenization for Karen
+pds.tokenize("သရၣ်,သရၣ်မုၣ် ခဲလၢာ်ဟးထီၣ် (၃၅) ဂၤန့ၣ်လီၤ.", lang="karen")
+>> ['ကၠိ', 'သ', 'ရၣ်', ',', 'သ', 'ရၣ်', 'မုၣ်', 'ခဲ', 'လၢာ်', 'ဟး', 'ထီၣ်', '(', '၃၅', ')', 'ဂၤ', 'န့ၣ်', 'လီၤ', '.']
+
+# word level tokenization
+pds.tokenize("ဖေဖေနဲ့မေမေ၏ကျေးဇူးတရားမှာကြီးမားလှပေသည်", form="word")
+>> ['ဖေဖေ', 'နဲ့', 'မေမေ', '၏', 'ကျေးဇူးတရား', 'မှာ', 'ကြီးမား', 'လှ', 'ပေ', 'သည်']
+
 ```
+
+Syllable-level tokenization supports for 4 languages (Burmese, Karen, Shan, Mon). Word-level tokenization supports only Burmese currently.</br>
+Available values for `lang` parameter in `tokenize` function: "mm", "karen", "mon", "shan"
 
 ## Future work
 
-- [ ] Add tokenizer for Burmese
+- [x] Add tokenizer for Burmese (Syllabel and word-level tokenization)
+- [ ] Add more tokenizer (BPE, WordPiece etc.)
 - [ ] Add Part-of-Speech (POS) tagger for Burmese
 - [ ] Add Named-entities Recognition (NER) classifier for Burmese
 - [ ] Add thorough documentation
---
+++ pyidaungsu/tokenize.py
@@ -0,0 +1,101 @@
+import re, os
+import pycrfsuite
+
+class Tokenize:
+    def __init__(self):
+        self.karen_consonant = 'ကခဂဃငစဆၡညတထဒနပဖဘမယရလဝသဟအဧၦ'
+        self.shan_consonant = 'ၵၶငၸသၹၺတထၼပၽၾမယလဝရႁဢၷႀၻၿ'
+        self.mon_consonant = 'ကခဂဃၚစဆဇၛညဋဌဍဎဏတထဒဓနပဖဗဘမယရလဝသဟဠၜအၝ'
+        self.burmese_consonant = 'က-အ'
+        self.others = '၊။!-/:-@[-`{-~\s.'
+
+    def create_char_features(self, sentence, i):
+        features = [
+        'bias',
+        'char=' + sentence[i][0] 
+        ]
+        
+        if i >= 1:
+            features.extend([
+                'char-1=' + sentence[i-1][0],
+                'char-1:0=' + sentence[i-1][0] + sentence[i][0],
+            ])
+        else:
+            features.append("BOS")
+            
+        if i >= 2:
+            features.extend([
+                'char-2=' + sentence[i-2][0],
+                'char-2:0=' + sentence[i-2][0] + sentence[i-1][0] + sentence[i][0],
+                'char-2:-1=' + sentence[i-2][0] + sentence[i-1][0],
+            ])
+            
+        if i >= 3:
+            features.extend([
+                'char-3:0=' + sentence[i-3][0] + sentence[i-2][0] + sentence[i-1][0] + sentence[i][0],
+                'char-3:-1=' + sentence[i-3][0] + sentence[i-2][0] + sentence[i-1][0],
+            ])
+            
+            
+        if i + 1 < len(sentence):
+            features.extend([
+                'char+1=' + sentence[i+1][0],
+                'char:+1=' + sentence[i][0] + sentence[i+1][0],
+            ])
+        else:
+            features.append("EOS")
+            
+        if i + 2 < len(sentence):
+            features.extend([
+                'char+2=' + sentence[i+2][0],
+                'char:+2=' + sentence[i][0] + sentence[i+1][0] + sentence[i+2][0],
+                'char+1:+2=' + sentence[i+1][0] + sentence[i+2][0],
+            ])
+            
+        if i + 3 < len(sentence):
+            features.extend([
+                'char:+3=' + sentence[i][0] + sentence[i+1][0] + sentence[i+2][0]+ sentence[i+3][0],
+                'char+1:+3=' + sentence[i+1][0] + sentence[i+2][0] + sentence[i+3][0],
+            ])
+        
+        return features
+
+    def create_sentence_features(self, prepared_sentence):
+        return [self.create_char_features(prepared_sentence, i) for i in range(len(prepared_sentence))]
+
+    def segment_sentence(self, tagger, sentence):
+        sent = sentence.replace(" ", "")
+        prediction = tagger.tag(self.create_sentence_features(sent))
+        complete = ""
+        for i, p in enumerate(prediction):
+            if p == "1":
+                complete += " " + sent[i]
+            else:
+                complete += sent[i]
+        return complete
+    
+    def tokenize(self, line, lang='mm', form='syllable'):
+
+        if form=='word':
+            return self.tokenize_word(line,lang,form)
+        if lang=='mm':
+            others = 'ဣဤဥဦဧဩဪဿ၌၍၏၀-၉၊။!-/:-@[-`{-~\s.,'
+            line = re.sub("(?<![္])(["+self.burmese_consonant+"])(?![်္])|(["+others+"])",r" \1\2", line).strip()
+        if lang=='karen':
+            line = re.sub("(["+self.karen_consonant+"])|(["+self.others+"])",r" \1\2", line).strip()
+        if lang=='shan':
+            line = re.sub("(["+self.shan_consonant+"])(?![်္])|(["+self.others+"])",r" \1\2", line).strip()
+        if lang=='mon':
+            line = re.sub("(?<![္])(["+self.mon_consonant+"])(?![်္])|(["+self.others+"])",r" \1\2", line).strip()
+
+        line = re.sub('(?<=[က-ၴ])([a-zA-Z0-9])',r' \1',line)
+        line = re.sub('([0-9၀-၉])\s+([0-9၀-၉])\s*',r'\1\2 ',line)
+        line = re.sub('([0-9၀-၉])\s+(\+)',r'\1 \2 ',line)
+
+        return line.split()
+    
+    def tokenize_word(self, line, lang, form):
+        tagger = pycrfsuite.Tagger()
+        model_path = os.path.join(os.path.abspath(os.path.dirname(__file__)),'model/tokenizer.crfsuite')
+        tagger.open(model_path)
+        return self.segment_sentence(tagger,line).strip().split()
